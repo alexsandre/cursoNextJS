685c04777cdcb96b6bddc8b3d72eabba86031deb
Alexsandre Almeida <alexsandrer@gmail.com>
2021-04-01 03:22:29 -0300

    Add meta tag - google search console

--- my-trips/src/pages/_app.tsx
+++ my-trips/src/pages/_app.tsx
@@ -11,6 +11,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="Xdg6rwzxEoMbXSX_in6eVffKNdek9lD-hm-OVMN2xIQ"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
